25e659f372bb0b4b7da4c7c30c382ca4373634db
Mohammed Ajil <mohammed@ajil.ch>
2017-10-01 16:39:34 +0000

    each version of each datapoint will be in one batch, batches restart after one epoch

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -13,12 +13,7 @@ class Dataset(object):
         """Construct Dataset
         
         Args:
-            config: A config of a datasource. The following fields must be provided:
-                config.base_path
-                config.base_name
-                config.preprocess_pipeline
-                config.num_datapoints
-                config.load_function
+            config: A DatasetConfig object
         
         Returns:
             Dataset
@@ -26,13 +21,29 @@ class Dataset(object):
         self.config = config
         
         if indices is None:
-            self.indices = rand.shuffle(range(1, config.num_datapoints+1))
+            self.indices = list(range(1, config.augmentation_multiplicator*config.num_datapoints+1))
+            rand.shuffle(self.indices)
         else:
             self.indices = indices
     
     def reset_batches(self):
         self.current_batch = 0
-        self.indices = rand.shuffle(range(1, self.config.num_datapoints+1))
+        rand.shuffle(self.indices)
+        
+    def get_data_point_id(self, batch_id):
+        data_point_id = batch_id % self.config.num_datapoints
+        if data_point_id == 0:
+            return self.config.num_datapoints
+        else:
+            return data_point_id
+
+    def get_data_point_version(self, batch_id, data_point_id):
+        return (batch_id / data_point_id) - 1
+        
+    def pad_batch(self, batch, batch_size):
+        diff = batch_size - len(batch)
+        for i in range(diff):
+            batch.append(batch[i % len(batch)])
 
     def get_next_batch(self, batch_size):
         """Returns the next batch of the dataset
@@ -43,17 +54,27 @@ class Dataset(object):
         Returns:
             The next batch of data in a form that is ready to be consumed by the model
         """
-        lower = self.current_batch*batch_size
-        upper = (self.current_batch+1)*batch_size
+        lower = min(self.current_batch*batch_size, self.config.num_datapoints)
+        upper = min((self.current_batch+1)*batch_size, self.config.num_datapoints)
+        
+        if lower == upper: # Epoch is done, start the next one
+            self.reset_batches()
+            return self.get_next_batch(batch_size)
+        
         batch_ids = self.indices[lower:upper]
         batch = []
-        for data_point_id in batch_ids:
+        for batch_id in batch_ids:
+            data_point_id = self.get_data_point_id(batch_id)
+            data_point_version = self.get_data_point_version(batch_id, data_point_id)
             data_point_file = os.path.join(self.config.base_name + data_point_id, self.config.base_path)
             data_point = self.config.load_function(data_point_file)
             processed_data_point = [data_point]
             for fun in self.config.preprocess_pipeline:
                 processed_data_point = map(fun, processed_data_point)
-            batch.append(rand.choice(processed_data_point))
+            batch.append(processed_data_point[data_point_version])
+        
+        if len(batch) < batch_size:
+            self.pad_batch(batch, batch_size)
         
         return batch
         
