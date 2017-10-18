dbe6f9cafc9ce339422ee272cde4bc1e8eb3a435
Mohammed Ajil <mohammed.ajil@digitecgalaxus.ch>
2017-10-18 09:17:12 +0200

    changed arg name to min images to reflect logic

--- downloaders/hirise.py
+++ downloaders/hirise.py
@@ -11,7 +11,7 @@ import os
 import time
 
 
-def gather_links(download_location, num_images):
+def gather_links(download_location, min_images):
     """Download HiRISE images
     """
     base_path = "https://hirise-pds.lpl.arizona.edu/PDS/EDR/ESP/"
@@ -24,7 +24,7 @@ def gather_links(download_location, num_images):
         if len(image_links) > 100:
             download_images(image_links, download_location)
             image_links = []
-        if len(os.listdir(download_location)) > num_images:
+        if len(os.listdir(download_location)) > min_images:
             return
         meta, response = http_client.request(base_path + level_0_link)
         level_1 = BeautifulSoup(response, "html5lib")
@@ -57,8 +57,8 @@ def download_images(image_links, download_location):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("download_location", metavar="download_location", type=str, help="Path to download images to")
-    parser.add_argument("--num_images", dest="num_images", type=int, help="Number of images to download", default=-1)
+    parser.add_argument("--min_images", dest="min_images", type=int, help="Minimum number of images to download", default=-1)
     args = parser.parse_args()
 
     os.makedirs(args.download_location)
-    gather_links(args.download_location, args.num_images)
+    gather_links(args.download_location, args.min_images)
