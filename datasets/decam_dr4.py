0c8477c2ed1e03a79cebd2da6a7d9c4bde5e149d
Mohammed Ajil <mohammed.ajil@digitecgalaxus.ch>
2017-11-01 21:40:47 +0100

    added runs, fixed data preprocessing

--- datasets/decam_dr4.py
+++ datasets/decam_dr4.py
@@ -8,19 +8,8 @@ import hashlib
 
 class Decam(Dataset):
 
-    def random_square(self, image):
-        randints = np.random.random_integers(0, self._config.input_dimensions.width, 4)
-        left = min(randints[:2])
-        right = max(randints[:2])
-        top = max(randints[2:])
-        bottom = min(randints[2:])
-        for i in range(left, right):
-            for j in range(bottom, top):
-                image[i, j, 0] = 1
-        return image
-
     def _preprocess_pipeline(self):
-        return []
+        return [lambda x: (x / 2**15) - 1, lambda x: -x]
 
     def set_seed(self, file_name):
         hash_string = hashlib.md5(file_name.encode()).hexdigest()
@@ -30,14 +19,10 @@ class Decam(Dataset):
     def _load_function(self, file_name):
         self.set_seed(file_name)
         file = os.path.join(self._config.base_path, file_name)
-        try:
-            image_data = fits.getdata(file, ignore_missing_end=True)
-        except OSError:
-            raise OSError("Image cannot be opened: {}".format(file_name))
+        image_data = fits.getdata(file, ignore_missing_end=True)
         height, width = image_data.shape
-        normalized_image = (np.log(image_data) / 15) - 1
         # plt.imshow(pdsimage.image, cmap='gray')
-        return np.reshape(normalized_image, (height, width, 1))
+        return np.reshape(image_data, (height, width, 1))
 
     def _crop_input(self, datapoint):
         height, width, depth = datapoint.shape
