340a304a5d00ded5bf6790622bdf06670dd4b7a5
Mohammed Ajil <mohammed.ajil@digitecgalaxus.ch>
2017-11-01 17:45:11 +0100

    moved validation images into log dir

--- models/pix2pix.py
+++ models/pix2pix.py
@@ -189,9 +189,12 @@ class Pix2pix(object):
         for image in batch_images:
             single_images.extend(np.split(image, self._config.batch_size))
 
+        if not os.path.exists('{}/images'.format(self._config.log_dir)):
+            os.makedirs('{}/images'.format(self._config.log_dir))
+
         for image_id, image in enumerate(single_images):
-            imsave('{}/validation_image_{}_{}.png'.format(self._config.log_dir, image_id, train_step), image, format='png')
-            encoded_image = open('{}/validation_image_{}_{}.png'.format(self._config.log_dir,image_id, train_step), 'rb').read()
+            imsave('{}/images/validation_image_{}_{}.png'.format(self._config.log_dir, image_id, train_step), image, format='png')
+            encoded_image = open('{}/images/validation_image_{}_{}.png'.format(self._config.log_dir, image_id, train_step), 'rb').read()
             images_summary.value.add(tag='validation_images/' + str(image_id), image=tf.Summary.Image(encoded_image_string=encoded_image))
 
         loss_summary.value.add(tag='avg_validation_loss', simple_value=avg_val_loss)
