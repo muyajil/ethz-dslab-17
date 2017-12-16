b27f199f01cfc7aad7b3be1009bea66830b80634
Mohammed Ajil <ajilm@phys.ethz.ch>
2017-12-16 15:51:15 +0100

    cleanup
    Former-commit-id: 0dc465abc0f9f41a25e9fbca4cd84e5fd66ecdf5

--- main.py
+++ main.py
@@ -61,15 +61,15 @@ if __name__ == "__main__":
     model = Res2pix(config=Config(args.batch_size,
                                     input_dimensions,
                                     args.log_dir,
-                                    pretrain_epochs=30,
+                                    pretrain_epochs=50,
                                     debug=True,
                                     gen_lambda=1,
-                                    learning_rate=0.002,
+                                    learning_rate=0.001,
                                     adam_beta1=0.9,
-                                    stages=4,
+                                    stages=8,
                                     show_jpeg=False,
                                     steps_between_val=50,
-                                    patch_size=256,
+                                    patch_size=128,
                                     conv_filter_size=3,
-                                    bitplane_depth=2))
+                                    bitplane_depth=8))
     run_model(args.epochs, args.split_ratio)
