fixed scaling
Former-commit-id: 5fc84eae89534a577b20f3056a31567428c9e893

datasets/imagenet.py
@@ -11,7 +11,7 @@ class ImageNet(Dataset):
 
     def _preprocess_pipeline(self):
         # lambda x: np.arcsinh(10*x)/3
-        return [lambda x: (x / 2**16), lambda x: 2*x-1]
+        return [lambda x: (x / 255), lambda x: 2*x-1]
 
     def set_seed(self, file_name):
         hash_string = hashlib.md5(file_name.encode()).hexdigest()