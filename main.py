1a01180d74d07dade2d752e4efe404f9dbff8b61
Vincent <svincent@student.ethz.ch>
2017-12-18 03:15:57 +0000

    no reconstr loss
    Former-commit-id: 308bedaabe2e202ccf678008b3b4d96f4638129b

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
                                     args.log_dir,
                                     pretrain_epochs=0,
                                     debug=True,
-                                    gen_lambda=1,
+                                    gen_lambda=0,
                                     learning_rate=0.001,
                                     adam_beta1=0.9,
                                     stages=8,
